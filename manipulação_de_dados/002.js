8d2a4d75f7f54cd19e7dc5961e96a3fa37963f65
ray-medeiros <109743028+ray-medeiros@users.noreply.github.com>
2022-10-02 22:22:00 -0300

    First commit

---
+++ manipulação_de_dados/002.js
@@ -0,0 +1,6 @@
+var n1 = Number.parseInt(window.prompt('Digite um número:'));
+var n2 = Number.parseFloat(window.prompt('Digite outro  número:'));
+var s = n1 + n2
+window.alert('A soma dos valores é:' + String(s));
+//number + number: adição
+//string + string: concatenação
